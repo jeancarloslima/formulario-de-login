fc52540d1c74e8923cc6e69423c434433f3603ce
Jean Lima <jcoliveiralima19@gmail.com>
2026-09-08 12:49:33 -0300

    Adicionando tipagem do typescript no card de perfil

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,4 +1,11 @@
-export default function ProfileCard({ name, lastName, email, password }) {
+interface ProfileCardProps {
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+export default function ProfileCard({ name, lastName, email, password }: ProfileCardProps) {
   return (
     <div className="w-full max-w-250 flex flex-col gap-4 p-6 bg-zinc-800 rounded-2xl shadow-2xl text-indigo-50">
       <h2 className="text-center text-3xl font-semibold">Perfil</h2>
