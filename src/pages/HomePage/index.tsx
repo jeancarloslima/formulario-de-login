a50823abcc176ef4dca5f8f31c0fcd498dbbc781
Jean Lima <jcoliveiralima19@gmail.com>
2026-09-09 12:47:45 -0300

    Chamando redirecionamento de página com useEffect

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,18 +1,26 @@
 import { useLocation, useNavigate } from "react-router";
 import ProfileCard from "../../components/ProfileCard";
+import { useEffect } from "react";
 
 export default function HomePage() {
   const location = useLocation();
   const navigate = useNavigate();
   const { name, lastName, email, password } = location.state || {};
 
-  if (!email) {
-    navigate('/auth/register');
-  }
+  useEffect(() => {
+    if (!email) {
+      navigate("/auth/register");
+    }
+  }, []);
 
   return (
     <div className="w-screen min-h-screen flex items-center justify-center p-4 bg-zinc-600">
-      <ProfileCard name={name} lastName={lastName} email={email} password={password} />
+      <ProfileCard
+        name={name}
+        lastName={lastName}
+        email={email}
+        password={password}
+      />
     </div>
   );
 }
